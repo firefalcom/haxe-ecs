512e36bf622cb64820d87ed1feed915ffcc0c45e
Gauthier Billot <gogoprog@gmail.com>
2024-11-30 13:22:48 +0100

    Add easier way to enable/disable systems

--- src/ecs/SystemList.hx
+++ src/ecs/SystemList.hx
@@ -5,50 +5,42 @@ import ecs.GenericListIterator;
 /**
  * Used internally, this is an ordered list of Systems for use by the engine update loop.
  */
-class SystemList
-{
+class SystemList {
     @:allow(ecs)
     var head:System;
 
     @:allow(ecs)
     var tail:System;
 
-    public function new()
-    {
+    public function new() {
     }
 
-    public function add(system:System):Void
-    {
-        if (head == null)
-        {
+    public function add(system:System):Void {
+        if(head == null) {
             head = tail = system;
             system.next = system.previous = null;
-        }
-        else
-        {
+        } else {
             var node:System = tail;
-            while (node != null)
-            {
-                if (node.priority <= system.priority)
+
+            while(node != null) {
+                if(node.priority <= system.priority) {
                     break;
+                }
+
                 node = node.previous;
             }
-            if (node == tail)
-            {
+
+            if(node == tail) {
                 tail.next = system;
                 system.previous = tail;
                 system.next = null;
                 tail = system;
-            }
-            else if (node == null)
-            {
+            } else if(node == null) {
                 system.next = head;
                 system.previous = null;
                 head.previous = system;
                 head = system;
-            }
-            else
-            {
+            } else {
                 system.next = node.next;
                 system.previous = node;
                 node.next.previous = system;
@@ -57,46 +49,56 @@ class SystemList
         }
     }
 
-    public function remove(system:System):Void
-    {
-        if (head == system)
+    public function remove(system:System):Void {
+        if(head == system) {
             head = head.next;
-        if (tail == system)
-            tail = tail.previous;
+        }
 
-        if (system.previous != null)
+        if(tail == system) {
+            tail = tail.previous;
+        }
+
+        if(system.previous != null) {
             system.previous.next = system.next;
-        if (system.next != null)
+        }
+
+        if(system.next != null) {
             system.next.previous = system.previous;
+        }
+
         // N.B. Don't set system.next and system.previous to null because that will break the list iteration if system is the current system in the iteration.
     }
 
-    public function removeAll():Void
-    {
-        while (head != null)
-        {
+    public function removeAll():Void {
+        while(head != null) {
             var system:System = head;
             head = head.next;
             system.previous = null;
             system.next = null;
         }
+
         tail = null;
     }
 
-    public function get<TSystem:System>(type:Class<TSystem>):TSystem
-    {
+    public function get<TSystem:System>(type:Class<TSystem>):TSystem {
         var system:System = head;
-        while (system != null)
-        {
-            if (Std.isOfType(system, type))
+
+        while(system != null) {
+            if(Std.isOfType(system, type)) {
                 return cast system;
+            }
+
             system = system.next;
         }
+
         return null;
     }
 
-    public inline function iterator():GenericListIterator<System>
-    {
+    public function contains<TSystem:System>(type:Class<TSystem>):Bool {
+        return get(type) != null;
+    }
+
+    public inline function iterator():GenericListIterator<System> {
         return new GenericListIterator(head);
     }
 }
